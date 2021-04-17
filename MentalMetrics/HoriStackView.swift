d47ee78df39f04b9c480d987563904163a151048
danielrevay <76134734+danielrevay@users.noreply.github.com>
2021-04-17 16:28:27 -0400

    small update

---
+++ MentalMetrics/HoriStackView.swift
@@ -0,0 +1,45 @@
+//
+//  HoriStackView.swift
+//  MentalMetrics
+//
+//  Created by Daniel Revay on 4/17/21.
+//
+
+import Foundation
+import UIKit
+
+class HoriStackView : UIViewController {
+    @IBOutlet weak var vwScrollView: UIScrollView!
+    var slides:[Slide] = [];
+    @IBOutlet weak var pageControl: UIPageControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        slides = createSlides()
+        setupSlideScrollView(slides: slides)
+        pageControl.numberOfPages = slides.count
+        pageControl.currentPage = 0
+        view.bringSubviewToFront(pageControl)
+    }
+    func createSlides() -> [Slide] {
+        let slide1:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide1.lblDate.text = "ur mom"
+        slide1.lblActualMood.text = "xd"
+        slide1.lblExpectedMood.text = "u"
+        let slide2:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide2.lblDate.text = "ur dad"
+        slide2.lblActualMood.text = "xyd"
+        slide2.lblExpectedMood.text = "ur"
+        return [slide1,slide2]
+    }
+    func setupSlideScrollView(slides:[Slide]){
+        vwScrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        vwScrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
+        vwScrollView.isPagingEnabled = true
+                
+        for i in 0 ..< slides.count {
+            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+            vwScrollView.addSubview(slides[i])
+        }
+    }
+}
---
+++ MentalMetrics/Slide.swift
@@ -0,0 +1,23 @@
+//
+//  Slide.swift
+//  MentalMetrics
+//
+//  Created by Daniel Revay on 4/17/21.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    @IBOutlet weak var lblDate: UILabel!
+    @IBOutlet weak var lblActualMood: UILabel!
+    @IBOutlet weak var lblExpectedMood: UILabel!
+    
+}
