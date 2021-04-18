413c6c0973a7514b9e3f465e6c69523bbce75fbb
danielrevay <76134734+danielrevay@users.noreply.github.com>
2021-04-18 01:37:51 -0400

    buttons

--- MentalMetrics/ViewController.swift
+++ MentalMetrics/ViewController.swift
@@ -18,30 +18,41 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    var clrOn = UIColor(hue: 0, saturation: 0, brightness: 0, alpha: 1)
+    var clrOff = UIColor(hue: 0, saturation: 0, brightness: 100, alpha: 1)
     @IBAction func pickGood(_ sender: Any) {
-        btnBad.titleLabel?.text = ""
-        btnOK.titleLabel?.text = ""
-        btnGood.titleLabel?.text = "Today was a good day!"
+        btnGood.setTitleColor(clrOn, for: UIButton.State.normal)
+        btnOK.setTitleColor(clrOff, for: UIButton.State.normal)
+        btnBad.setTitleColor(clrOff,for: UIButton.State.normal)
+        btnBad.setTitle("", for: UIButton.State.normal)
+        btnOK.setTitle("", for: UIButton.State.normal)
+        btnGood.setTitle("Today was a good day!", for: UIButton.State.normal)
         selected = 0
         print("0")
     }
     @IBAction func pickOK(_ sender: Any) {
-        btnBad.titleLabel?.text = ""
-        btnOK.titleLabel?.text = "Today was fine."
-        btnGood.titleLabel?.text = ""
+        btnGood.setTitleColor(clrOff, for: UIButton.State.normal)
+        btnOK.setTitleColor(clrOn, for: UIButton.State.normal)
+        btnBad.setTitleColor(clrOff,for: UIButton.State.normal)
+        btnBad.setTitle("", for: UIButton.State.normal)
+        btnOK.setTitle("Today was fine.", for: UIButton.State.normal)
+        btnGood.setTitle("", for: UIButton.State.normal)
         selected = 1
         print("1")
     }
     @IBAction func pickBad(_ sender: Any) {
-        btnBad.titleLabel?.text = "Today could have been better."
-        btnOK.titleLabel?.text = ""
-        btnGood.titleLabel?.text = ""
+        btnGood.setTitleColor(clrOff, for: UIButton.State.normal)
+        btnOK.setTitleColor(clrOff, for: UIButton.State.normal)
+        btnBad.setTitleColor(clrOn,for: UIButton.State.normal)
+        btnBad.setTitle("Today could have been better.", for: UIButton.State.normal)
+        btnOK.setTitle("", for: UIButton.State.normal)
+        btnGood.setTitle("", for: UIButton.State.normal)
         selected = 2
         print("2")
     }
     @IBAction func pickSUBMIT(_ sender: Any) {
         //This is the part where you run away
+        //it also does nothing
     }
     override func prepare(for segue: UIStoryboardSegue,sender: (Any)?){
         if segue.identifier == "submit" {
